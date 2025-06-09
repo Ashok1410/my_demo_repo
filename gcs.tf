1c9a9c1c2f8b8eb818ccb9c0283b6887e9cbd2ba
Ashok1410 <ashok14btech@gmail.com>
2025-06-09 21:01:45 +0530

    gcs_to_bq_and_iam

--- gcs.tf
+++ gcs.tf
@@ -1,12 +1,12 @@
 resource "google_storage_bucket" "my_data_bucket" {
-  name     = "${var.project_id}-bq-data-${random_id.suffix.hex}"  # ensures global uniqueness
+  name     = "${var.project_id}-bq-data-d69becfc"  # ensures global uniqueness
   location = "${var.location}"  # or your preferred region like "ASIA-SOUTH1"
   force_destroy = true  # allows Terraform to delete non-empty bucket
 
   uniform_bucket_level_access = true
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
-}
+#resource "random_id" "suffix" {
+#  byte_length = 4
+#}
 
