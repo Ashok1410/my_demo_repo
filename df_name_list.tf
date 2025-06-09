c030e86bae81a03b2d8105242f05ba5f52335084
Ashok1410 <ashok14btech@gmail.com>
2025-06-09 23:01:50 +0530

    gcs_to_bq_and_iam

--- df_name_list.tf
+++ df_name_list.tf
@@ -1,13 +1,16 @@
 resource "google_dataflow_job" "big_data_job" {
   name              = "dataflow-job" # df job name
-  template_gcs_path = "gs://dataflow-templates/latest/GCS_Text_to_BigQuery"
+  template_gcs_path = "gs://dataflow-templates/latest/GCS_CSV_to_BigQuery"
   temp_gcs_location = "gs://copper-eye-461406-a8-bq-data-d69becfc/Temp"
   
   parameters = {
     inputFilePattern                   = "gs://copper-eye-461406-a8-bq-data-d69becfc/name_list.csv"
     outputTable                        = "${var.project_id}:${var.dataset_id}.name_list"
     bigQueryLoadingTemporaryDirectory = "gs://copper-eye-461406-a8-bq-data-d69becfc/dataflow/bq_temp"
-    JSONPath                          = "gs://copper-eye-461406-a8-bq-data-d69becfc/schema.json"
+    schemaJSONPath                          = "gs://copper-eye-461406-a8-bq-data-d69becfc/schema.json"
+    csvFormat                          = "Default"
+    delimiter                          = ","
+    badRecordsOutputTable              = "${var.project_id}:${var.dataset_id}.bad_records"
   }
 
   on_delete = "cancel" # or "drain"
